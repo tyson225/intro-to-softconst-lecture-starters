094e47c0bf0b8b178eb19b198e5700cb5e308e9f
tyson <tyson22509@hotmail.com>
2019-05-31 11:04:34 -0400

    Update Shape.java

--- SimpleDrawingPlayer/src/model/Shape.java
+++ SimpleDrawingPlayer/src/model/Shape.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class Shape {
 
-    private static final Color PLAYING_COLOR = new Color(230, 158, 60);
+    private static final Color PLAYING_COLOR = new Color(51, 56, 230);
 
     private int x;
     private int y;
